87e6ca25fc7a080d9c0d2b0e11087124b7e28474
Miguel-Horta <miguelattero@hotmail.com>
2022-11-25 09:55:06 -0600

    Updated methods to add books and not repeat divs, code is a mess but it works except the delete button, still working on it

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const harryPotter1 = new makeBook("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "400","Yes");
 console.log(harryPotter1.info());
 let myLibrary = [];
+document.getElementById("deleteBtn").onclick = function(){deleteBook(this)};
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
 
 function makeBook(title, author, pages, readedBook){
     this.title = title;
@@ -21,32 +26,67 @@ function newBook() {
     return;
   }
   
-  if(myLibrary.lenght !== 0)
+  if(myLibrary.length !== 0 && document.getElementById("name").value !== "")
   {
     const books = document.querySelector('#books');
     const clone = books.cloneNode(true);
     clone.id = 'book';
     books.after(clone);
+    document.getElementById("name").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.querySelector("#readed").checked = false;
+    document.getElementById("name").disabled = false;
+    document.getElementById("author").disabled = false;
+    document.getElementById("pages").disabled = false;
+    document.querySelector("#readed").disabled = false;
+    document.getElementById("saveBtn").disabled = false;
+    document.getElementById("saveBtn").textContent = "Save";
+    document.getElementById("deleteBtn").style.visibility = "hidden";
   } 
 }
 
 function saveValues(){
   const title = document.getElementById("name").value;
+  if (isEmpty(title) === true)
+  {
+    alert("Book name cannot be empty")
+    return;
+  }
   const author = document.getElementById("author").value;
+  if (isEmpty(author) === true)
+  {
+    alert("Author name cannot be empty")
+    return;
+  }
   const pages = document.getElementById("pages").value;
+  if (isEmpty(pages) === true)
+  {
+    alert("A book without pages?")
+    return;
+  }
   const readed = document.querySelector("#readed").checked;
+  
   const book = new makeBook(title,author,pages,readed);
-  myLibrary.push(book);
-  console.log(myLibrary);
-
+  myLibrary.push(book); 
   document.getElementById("name").disabled = true;
   document.getElementById("author").disabled = true;
   document.getElementById("pages").disabled = true;
   document.querySelector("#readed").disabled = true;
+  document.getElementById("saveBtn").disabled = true;
+  document.getElementById("saveBtn").textContent = "Saved";
+  document.getElementById("deleteBtn").style.visibility = "visible";
+}
+
+function isEmpty(str) {
+  return !str.trim().length;
 }
 
 const isHidden = elem => {
-
   const styles = window.getComputedStyle(elem)
   return styles.display === 'none' || styles.visibility === 'hidden'
+}
+
+const deleteBook = name => {
+  alert("You deleted book: " + name);
 }
